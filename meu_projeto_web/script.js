09572c25038115cc83a47f11a2a51a20b59970ea
Vanderson Souza <Coutinho.vanderson@hotimail.com>
2026-05-24 12:42:22 -0300

    primeiro arquivo

--- meu_projeto_web/script.js
+++ meu_projeto_web/script.js
@@ -1,1 +1,25 @@
-console.log("Sistema iniciado!");
+function fazerLogin(){
+
+    // Captura os valores
+    let usuario = document.getElementById("usuario").value;
+
+    let senha = document.getElementById("senha").value;
+
+    let mensagem = document.getElementById("mensagem");
+
+    // Usuário teste
+    if(usuario === "lele" && senha === "1981"){
+
+        mensagem.style.color = "green";
+
+        mensagem.innerHTML = "Login realizado com sucesso!";
+
+    }else{
+
+        mensagem.style.color = "red";
+
+        mensagem.innerHTML = "Usuário ou senha inválidos";
+
+    }
+
+}
